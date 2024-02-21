12818d38aa0174f17df224a32809e78f5160342a
benny <benny.ma@compartsys.com>
2024-02-21 15:38:51 +0800

    屏幕与标签比例适配

---
+++ app/src/main/java/com/epicor/canvas/PrintContent.java
@@ -0,0 +1,83 @@
+package com.epicor.canvas;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.epicor.canvas.R;
+import com.gprinter.command.LabelCommand;
+
+import java.util.Vector;
+
+/**
+ * Copyright (C), 2012-2020, 珠海佳博科技股份有限公司
+ * FileName: PrintConntent
+ * Author: Circle
+ * Date: 2020/7/20 10:04
+ * Description: 打印内容
+ */
+public class PrintContent {
+    /**
+     * 获取图片
+     * @param context
+     * @return
+     */
+    @SuppressLint("SetTextI18n")
+    public static Bitmap getBitmap(Context context, Bitmap mBitmap ) {
+        View v = View.inflate(context, R.layout.lam_page, null);
+
+        ImageView mNum1 = (ImageView) v.findViewById(R.id.mImage);
+        mNum1.setImageBitmap(mBitmap);
+        mNum1.setScaleType(ImageView.ScaleType.FIT_XY);
+
+
+        final Bitmap bitmap = convertViewToBitmap(v);
+        return bitmap;
+    }
+
+    /**
+     * mxl转bitmap图片
+     * @return
+     */
+    public static Bitmap convertViewToBitmap(View view){
+        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache();
+        Bitmap bitmap = view.getDrawingCache();
+        return bitmap;
+    }
+
+
+    public static Vector<Byte> getXmlBitmap(Context context,Bitmap mBitmap ){
+        LabelCommand tsc = new LabelCommand();
+        // 设置标签尺寸宽高，按照实际尺寸设置 单位mm
+        tsc.addUserCommand("\r\n");
+        tsc.addSize(110, 68);
+        // 设置标签间隙，按照实际尺寸设置，如果为无间隙纸则设置为0 单位mm
+        tsc.addGap(2);
+        // 设置打印方向
+        tsc.addDirection(LabelCommand.DIRECTION.FORWARD, LabelCommand.MIRROR.NORMAL);
+        // 设置原点坐标
+        tsc.addReference(0, 0);
+        tsc.addSpeed(LabelCommand.SPEED.SPEED12);
+        //设置浓度
+        tsc.addDensity(LabelCommand.DENSITY.DNESITY10);
+        // 撕纸模式开启
+        tsc.addTear(LabelCommand.RESPONSE_MODE.ON);
+        // 清除打印缓冲区
+        tsc.addCls();
+        Bitmap bitmap=getBitmap(context,mBitmap);
+        // 绘制图片
+        tsc.drawXmlImage(0,0,1210,bitmap);
+//        tsc.drawImage(0,0,1210,mBitmap);
+        // 打印标签
+        tsc.addPrint(1);
+        return tsc.getCommand();
+    }
+
+
+}
--- app/src/main/java/com/epicor/canvas/ProportionActivity.kt
+++ app/src/main/java/com/epicor/canvas/ProportionActivity.kt
@@ -1,16 +1,26 @@
 package com.epicor.canvas
 
+import android.app.AlertDialog
 import android.content.res.Resources
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Base64
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.blankj.utilcode.util.ToastUtils
 import com.epicor.canvas.databinding.ActivityProportionBinding
+import com.gprinter.utils.Command
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
@@ -20,19 +30,126 @@ import java.io.InputStream
 
 class ProportionActivity : AppCompatActivity() {
 
+    private var printer: Printer? = null
+    private val portManager = Printer.getPortManager()
+
     private lateinit var binding: ActivityProportionBinding
 
     private var mProportion = 0.0
+
+
+    private var handler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                0x00 -> {
+                    val tip = msg.obj as String
+                    Toast.makeText(this@ProportionActivity, tip, Toast.LENGTH_SHORT).show()
+                }
+
+                0x01 -> {
+                    when (msg.arg1) {
+                        -1 -> { //获取状态失败
+                            val alertDialog = AlertDialog.Builder(this@ProportionActivity)
+                                .setTitle(getString(R.string.tip))
+                                .setMessage(getString(R.string.status_fail))
+                                .setIcon(R.mipmap.ic_launcher)
+                                .setPositiveButton(
+                                    getString(R.string.ok)
+                                ) //添加"Yes"按钮
+                                { _, _ -> }
+                                .create()
+                            alertDialog.show()
+                            return
+                        }
+
+                        1 -> {
+                            Toast.makeText(
+                                this@ProportionActivity,
+                                getString(R.string.status_feed),
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                            return
+                        }
+
+                        0 -> { //状态正常
+                            Toast.makeText(
+                                this@ProportionActivity,
+                                getString(R.string.status_normal),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            return
+                        }
+
+                        -2 -> { //状态缺纸
+                            Toast.makeText(
+                                this@ProportionActivity,
+                                getString(R.string.status_out_of_paper),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            return
+                        }
+
+                        -3 -> { //状态开盖
+                            Toast.makeText(
+                                this@ProportionActivity,
+                                getString(R.string.status_open),
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                            return
+                        }
+
+                        -4 -> {
+                            Toast.makeText(
+                                this@ProportionActivity,
+                                getString(R.string.status_overheated),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            return
+                        }
+                    }
+                }
+
+                0x02 -> {
+                    Thread {
+                        if (portManager != null) {
+                            Printer.close()
+                            Printer.getInstance().devices
+                        }
+                    }.start()
+//                    tvState.setText(getString(R.string.not_connected))
+                }
+
+                0x03 -> {
+                    val message = msg.obj as String
+                    val alertDialog = AlertDialog.Builder(this@ProportionActivity)
+                        .setTitle(getString(R.string.tip))
+                        .setMessage(message)
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setPositiveButton(
+                            getString(R.string.ok)
+                        ) //添加"Yes"按钮
+                        { _, _ -> }
+                        .create()
+                    alertDialog.show()
+                }
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProportionBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        initData()
+
         try {
             val inputStream: InputStream = this.assets.open("test1.html")
             val document: Document = Jsoup.parse(inputStream, "UTF-8", "") // 解析 HTML
-
             // 获取整个界面宽高
             val widthOrHeight: Elements = document.select("AreaSize")
             val regex = Regex("\\d+") // 匹配一个或多个数字
@@ -45,25 +162,19 @@ class ProportionActivity : AppCompatActivity() {
             } else {
                 println("Jsoup 无法从字符串中提取出足够的数字。")
             }
-
             // 获取 标签宽高
             val labelWidth: String = document.select("LabelPage").select("LabelWidth").text()
             val labelHeight: String = document.select("LabelPage").select("LabelHeight").text()
             // 获取 计算比例
             mProportion = mWith / labelWidth.toDouble()
-
             val mVCanvas = binding.mCanvas?.layoutParams
             mVCanvas?.width = labelWidth.toDouble().toInt().mmToPx()
             mVCanvas?.height = labelHeight.toDouble().toInt().mmToPx()
             binding.mCanvas?.layoutParams = mVCanvas
-
-            // 获取 标签子类数量
             val mNumber = document.select("ObjectList").attr("Count").toInt()
-
             val drawObject = document.select("DrawObject")
             for (i in 0 until mNumber) {
                 val mType = drawObject[i].select("Name").text()
-
                 if (mType.contains("WinText")) {
                     val mTextView = TextView(this)
                     mTextView.text = drawObject[i].select("OriginalData").text()
@@ -72,18 +183,14 @@ class ProportionActivity : AppCompatActivity() {
                     if (drawObject[i].select("Font").attr("FontStyle").contains("Bold")) {
                         mTextView.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD)
                     }
-
                     val layoutParams = RelativeLayout.LayoutParams(
                         RelativeLayout.LayoutParams.WRAP_CONTENT,
                         RelativeLayout.LayoutParams.WRAP_CONTENT
                     )
                     layoutParams.leftMargin = (drawObject[i].select("StartX").text().toInt() / mProportion).toInt().mmToPx()
                     layoutParams.topMargin = (drawObject[i].select("StartY").text().toInt() / mProportion).toInt().mmToPx()
-
                     binding.mCanvas?.addView(mTextView, layoutParams)
-
                 }
-
                 if (mType.contains("Barcode")) {
                     val mImageView = ImageView(this)
                     val decodedBytes: ByteArray = Base64.decode(drawObject[i].select("BarcodeImage").text(), Base64.DEFAULT)
@@ -101,7 +208,6 @@ class ProportionActivity : AppCompatActivity() {
 
                     binding.mCanvas?.addView(mImageView,layoutParams)
                 }
-
                 if (mType.contains("Image")){
                     val mImageView = ImageView(this)
                     val decodedBytes: ByteArray = Base64.decode(drawObject[i].select("OriginalImage").text(), Base64.DEFAULT)
@@ -114,9 +220,7 @@ class ProportionActivity : AppCompatActivity() {
                     )
                     layoutParams.leftMargin = (drawObject[i].select("StartX").text().toInt() / mProportion).toInt().mmToPx()
                     layoutParams.topMargin = (drawObject[i].select("StartY").text().toInt() / mProportion).toInt().mmToPx()
-
                     mImageView.scaleType = ImageView.ScaleType.CENTER_CROP
-
                     binding.mCanvas?.addView(mImageView,layoutParams)
                 }
             }
@@ -125,11 +229,68 @@ class ProportionActivity : AppCompatActivity() {
         } catch (e: IOException) {
             e.printStackTrace()
         }
+
+        binding.mPrint?.setOnClickListener {
+
+
+            repeat(1) {
+                val thread = Thread {
+                    portManager?.let {
+                        Log.e("测试是否进来","进来了1")
+                        //打印前后查询打印机状态，部分老款打印机不支持查询请去除下面查询代码
+                        val command: Command = Printer.getPrinterCommand()
+                        val status: Int = Printer.getPrinterState(command, 2000)
+                        if (status != 0) { //打印机处于不正常状态,则不发送打印任务
+                            val msg = Message()
+                            msg.what = 0x01
+                            msg.arg1 = status
+                            handler.sendMessage(msg)
+                            return@Thread
+                        }
+                        val bitmap = binding.mCanvas?.let { it1 -> getBitmapFromView(it1) }
+                        //***************************************************************
+                        val result: Boolean = portManager.writeDataImmediately(
+                            PrintContent.getXmlBitmap(this,bitmap)
+                        )
+
+                        runOnUiThread {
+                            // 在这里更新UI
+                            binding.mImage?.setImageBitmap(bitmap)
+                        }
+
+
+                        if (result) {
+                            ToastUtils.showShort(getString(R.string.send_success))
+                        } else {
+                            ToastUtils.showShort(getString(R.string.send_fail))
+                        }
+                    }
+                }
+                thread.start()
+                thread.join() // 等待当前线程执行完毕
+            }
+
+
+        }
     }
+
+    private fun getBitmapFromView(view: View): Bitmap? {
+        val mCanvas = binding.mCanvas
+        mCanvas?.isDrawingCacheEnabled = true
+        mCanvas?.buildDrawingCache()
+        val bitmap = mCanvas?.drawingCache?.let { Bitmap.createBitmap(it) }
+        mCanvas?.isDrawingCacheEnabled = false
+        return  bitmap
+    }
+
 
     private fun Int.mmToPx(): Int {
         val dpi = Resources.getSystem().displayMetrics.densityDpi
         return this * dpi / 31
+    }
+
+    private fun initData() {
+        printer = Printer.getInstance()
     }
 
 }
